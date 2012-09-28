1273e6fb5abc73bdab117f9ddd8e24bc9016402b
Nick Gresham <n.gresham@uq.edu.au>
2012-09-28 15:36:21 +1000

    Fix: https://github.com/thbar/bootstrap/commit/92189b87ad9e1f46966d5d730e3d8f99f969d92c

--- bootstrap-typeahead.js
+++ bootstrap-typeahead.js
@@ -292,12 +292,16 @@
       }
     }
 
-  , blur: function (e) {
+   , blur: function (e) {
       var that = this
-      e.stopPropagation()
-      e.preventDefault()
-      setTimeout(function () { that.hide() }, 150)
-    }
+	  e.stopPropagation()
+	  e.preventDefault()
+	  setTimeout(function () {
+	     if (!that.$menu.is(':hover')) {
+	        that.hide();
+	     }
+	  }, 150)
+  }
 
   , click: function (e) {
       e.stopPropagation()
